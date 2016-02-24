52897e0e7830ceb7f546c61e7a09b2fe76d4565f
sandip <sandip.iem@gmail.com>
2016-02-24 17:46:58 +0530

    fixing cv update model issue and added simple svm

--- amp_macro_19_Feb/main_macro_svm.m
+++ amp_macro_19_Feb/main_macro_svm.m
@@ -1,4 +1,4 @@
-clear;
+% clear;
 clc;
 
 [config] = setConfig();
@@ -20,9 +20,9 @@ fprintf(fid_parameters,'\n------------------------------------------------------
 %% Reading Training File
 disp('Reading Training file');
 
-datasize_avg = 12000;
+datasize_avg = 6000;
 no_of_classes = 4;
-[data, datasize] = generateTrainData(datasize_avg,1);
+% [data, datasize] = generateTrainData(datasize_avg,1);
 
 train_cnt_tot = 1:floor(datasize*2/3);
 test_cnt = floor(datasize*2/3)+1:datasize;
@@ -104,51 +104,48 @@ for curr_c = config.c_range
         config.w_0_cost=0.5;
         iteration_no = 0;
         %% AMP
-        while ((v_trn==0 && iteration_no < 10) || (abs(v_trn-v_prev) > .00001 &&  abs(v_trn-v_prev_prev) > .00001)) && (iteration_no < 10)
-            iteration_no = iteration_no +1;
-            
-            v_prev_prev = v_prev;
-            v_prev = v_trn;
-            
-            %%% this matrix will store the prediction obtained by SVM params
-            predicted_matrix = zeros(NO_OF_RELNS,count_of_sentences);
-            
-            
-            %%%find the first non zero label (SVM should have the first label non-zero, libsvm issue)
-            nz_row = find(gold_db_matrix_trn(:,i),1);
-            [model_final] = trainSVM_macro([1;gold_db_matrix_trn(:,i)], 1, [feature_vect_trn(nz_row,:); feature_vect_trn], config);
-            
-            %%%pridiction for the current relation
-            
-            [predicted_matrix_trn_temp, accuracy_trn, decision_values_trn_temp] = svmpredict([1;gold_db_matrix_trn(:,i)], [feature_vect_trn(nz_row,:);feature_vect_trn], model_final);
-            predicted_matrix_trn(i,:) = predicted_matrix_trn_temp(2:end,:);
-            
-            
-            %% Multiclass prediction
-            %%Calculate TP-TN-F_Score-W0-W1
-            [TP_macro_trn, TN_macro_trn] = find_TP_TN_macro(gold_db_matrix_trn(:,i), predicted_matrix_trn(i,:)')
-            [v_trn] = find_FScore_macro(config, TP_macro_trn, TN_macro_trn, Theta_macro_trn_vect(i,1));
-            v_trn_obj = (1+config.BETA^2-v_trn)*TP_macro_trn + v_trn*Theta_macro_trn_vect(i,1)*TN_macro_trn;
-            
-            %%write model params
-            writeModelParams(config,'w_1_cost',config.w_1_cost);
-            writeModelParams(config,'w_0_cost',config.w_0_cost);
-            writeModelParams(config,'v_trn',v_trn);
-            writeModelParams(config,'v_trn_obj',v_trn_obj);
-            fprintf(fid_parameters,'\n,----------------,\n');
-            
-            %%Update weights
-            config.w_1_cost=1+config.BETA^2-v_trn;
-            config.w_0_cost=v_trn*Theta_macro_trn_vect(i,1);
-            denom = config.w_1_cost+config.w_0_cost;
-            config.w_1_cost = config.w_1_cost/denom;
-            config.w_0_cost = config.w_0_cost/denom;
-%             config.model_final = model_final;
-            if v_trn==0 
-                break;
-            end
-            
-        end %%end of while
+        
+        iteration_no = iteration_no +1;
+        
+        v_prev_prev = v_prev;
+        v_prev = v_trn;
+        
+        %%% this matrix will store the prediction obtained by SVM params
+        predicted_matrix = zeros(NO_OF_RELNS,count_of_sentences);
+        
+        
+        %%%find the first non zero label (SVM should have the first label non-zero, libsvm issue)
+        nz_row = find(gold_db_matrix_trn(:,i),1);
+        [model_final] = trainSVM_macro([1;gold_db_matrix_trn(:,i)], 1, [feature_vect_trn(nz_row,:); feature_vect_trn], config);
+        
+        %%%pridiction for the current relation
+        
+        [predicted_matrix_trn_temp, accuracy_trn, decision_values_trn_temp] = svmpredict([1;gold_db_matrix_trn(:,i)], [feature_vect_trn(nz_row,:);feature_vect_trn], model_final);
+        predicted_matrix_trn(i,:) = predicted_matrix_trn_temp(2:end,:);
+        
+        
+        %% Multiclass prediction
+        %%Calculate TP-TN-F_Score-W0-W1
+        [TP_macro_trn, TN_macro_trn] = find_TP_TN_macro(gold_db_matrix_trn(:,i), predicted_matrix_trn(i,:)')
+        [v_trn] = find_FScore_macro(config, TP_macro_trn, TN_macro_trn, Theta_macro_trn_vect(i,1));
+        v_trn_obj = (1+config.BETA^2-v_trn)*TP_macro_trn + v_trn*Theta_macro_trn_vect(i,1)*TN_macro_trn;
+        
+        %%write model params
+        writeModelParams(config,'w_1_cost',config.w_1_cost);
+        writeModelParams(config,'w_0_cost',config.w_0_cost);
+        writeModelParams(config,'v_trn',v_trn);
+        writeModelParams(config,'v_trn_obj',v_trn_obj);
+        fprintf(fid_parameters,'\n,----------------,\n');
+        %
+        %             %%Update weights
+        %             config.w_1_cost=1+config.BETA^2-v_trn;
+        %             config.w_0_cost=v_trn*Theta_macro_trn_vect(i,1);
+        %             denom = config.w_1_cost+config.w_0_cost;
+        %             config.w_1_cost = config.w_1_cost/denom;
+        %             config.w_0_cost = config.w_0_cost/denom;
+% %         config.model_final = model_final;
+        
+        
         
         %% Set best params from cross validation
         %%%find the first non zero label (SVM should have the first label non-zero, libsvm issue)
@@ -177,14 +174,14 @@ for curr_c = config.c_range
     end %% end of for loop all relations
     
     
-%% Testing
-Theta_macro_test_vect = calculateThetaMacro(gold_db_matrix_test);
-[f_score] = test_macro_multi_class(config, gold_db_matrix_test, feature_vect_test, Theta_macro_test_vect);
-
-%% Write params to file
-writeModelParams(config, 'Test_f_score',f_score);
-fid_parameters = fopen(config.resultFile, 'a');
-fprintf(fid_parameters,'\n,------------------------------------------------,\n');
+    %% Testing
+    Theta_macro_test_vect = calculateThetaMacro(gold_db_matrix_test);
+    [f_score] = test_macro_multi_class(config, gold_db_matrix_test, feature_vect_test, Theta_macro_test_vect);
+    
+    %% Write params to file
+    writeModelParams(config, 'Test_f_score',f_score);
+    fid_parameters = fopen(config.resultFile, 'a');
+    fprintf(fid_parameters,'\n,------------------------------------------------,\n');
     
 end          %%%end of c loop
 
