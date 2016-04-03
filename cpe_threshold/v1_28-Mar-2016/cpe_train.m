122bab86e56bf31d8c3749b33824bae3c448eff7
sandip <sandip.iem@gmail.com>
2016-04-04 00:45:41 +0530

    basic checkin with epochs

--- cpe_threshold/v1_28-Mar-2016/cpe_train.m
+++ cpe_threshold/v1_28-Mar-2016/cpe_train.m
@@ -2,7 +2,7 @@
 %%% y_labels - binary matrix of sntnce X reln (hiddenlabels)
 %%%
 
-function [models, thresholds] = cpe_train(config, y_labels)
+function [thresholds, config] = cpe_train(config, y_labels)
 
 [yrow, ycol] = size(y_labels);
 
@@ -13,25 +13,45 @@ thresholds = zeros(1,ycol);
 for i=1:ycol
     
     %% @todo set the first training data as +ve example
-    
+    pstv_idx = find(y_labels(:,i)>0,1);
     
     %% @todo cross validation
     
+    max_f_score = 0;
+    thersh_idx = 1;
     
-    %% @todo need to set the c-range
-    params = ['-s 0 -b 1 -t 0  -c 1']
-    
-    
-    %% get the model for curr reln
-    models(i) = svmtrain(y_labels(:,i), config.feature_vect, params);
-    
-    %% predict cpe's
-    [predictions, accuracy_trn, cpe] = svmpredict(y_labels(:,i), config.feature_vect, models(i), '-b 1');
-    
-    
-    %% choose the best threshold
-    [thresholds(1,i), max_f_score]  = find_best_thresh(config, cpe,i);
-    
+    for c=config.c_range
+        
+        %% set the c-range
+        params = ['-s 0 -b 1 -t 0 -c ', num2str(2^c)]
+        
+        
+        %% get the model for curr reln
+        models(i) = svmtrain([y_labels(pstv_idx,i); y_labels(:,i)], [config.feature_vect(pstv_idx,:); config.feature_vect], params);
+        
+        %% predict cpe's
+        [predictions, accuracy_trn, cpe] = svmpredict(y_labels(:,i), config.feature_vect, models(i), '-b 1');
+        
+        
+        %% choose the best threshold
+        [curr_thresholds, f_score]  = find_best_thresh(config, cpe,i);
+        
+        if(f_score > max_f_score)
+            
+            %update best thresh
+            thresholds(1,i) = curr_thresholds;
+            max_f_score = f_score;
+            
+            %needs best models for testing
+            config.models(i) = models(i);
+            
+            %needs curr best cpe to predict next y_labels
+            config.cpe(:,i) = cpe(:,1);
+            
+        end
+        
+        config.testfsc_c(i,c) = f_score;
+    end
     
 end
 
--- cpe_threshold/v1_28-Mar-2016/main.m
+++ cpe_threshold/v1_28-Mar-2016/main.m
@@ -1,24 +1,58 @@
 clc; 
-setConfig();
+config = setConfig();
 
 %% generate synth data
+
 % sentence label (1000 snts X 10 dim)
 config.feature_vect = rand(1000,10)*10;
+
 % sentence label hidden vars (1000 snts X relations [binary labels])
 y_labels = round(rand(1000,5));
+
 % ent label (ent_pair X relns)
 config.gold_y_labels = round(rand(200,5));
+
 % mentions/ent_pair (5 each here)
 config.ent_mntn_cnt = zeros(200,1) + 5;
 
 
+%% @todo - read naacl files
+
+
+
 %% train the model
-[models, thresholds]  = cpe_train(config, y_labels);
+
+NO_OF_RELN = size(y_labels,2);
+
+%%call cpe with initial labels
+[thresh, config]  = cpe_train(config, y_labels);
+
+
+%% update mentionlabels as per the curr thresh & cpe
+
+%@todo - decide the stopping criteria
+for i = 1:config.EPOCHS_COUNT
+    
+    %update y labels as per the curr thresholds
+    
+    y_labels = config.cpe.*0;
+    
+    for r = 1:NO_OF_RELN
+        y_labels(config.cpe(:,r)>thresh(1,r),r) = 1;
+    end
+    
+    [thresh, config]  = cpe_train(config, y_labels);
+    
+end
+
+
+%% @todo - write the tesing code
 
 
 
 
 
+%% end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
